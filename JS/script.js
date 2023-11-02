6672e725d7fa6d23de3dd7a80db6caa4edff1e8e
Ryan Nascimento <ryan.nscmento@gmail.com>
2023-11-01 23:20:12 -0300

    randomization functions created

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,28 @@ const generatePasswordButton = document.getElementById('generate-password')
 const generatePasswordElement = document.getElementById('generated-password')
 
 //Funções
+const getLowerCaseLetter = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+const getUpperCaseLetter = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
+const getNumber = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48).toString()
+}
+
+const getSymbols = () => {
+    const symbols = "!@#$%¨&*()_+`{^}<>:?[~],.;/-=";
+    return symbols[Math.floor(Math.random() * 29)];
+}
 
 //Eventos
 
 generatePasswordButton.addEventListener("click", () => {
-    console.log("teste")
+    console.log(getSymbols())
+    generatePasswordElement.style.display = "block"   
     
 })
+
