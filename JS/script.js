07a621f6d646d7569aeca9937c562cb038b119e0
Ryan Nascimento <ryan.nscmento@gmail.com>
2023-11-01 22:51:19 -0300

    Add event to script, improvement in html and css

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+//Seleção de Elementos
+const generatePasswordButton = document.getElementById('generate-password')
+const generatePasswordElement = document.getElementById('generated-password')
+
+//Funções
+
+
+
+//Eventos
+
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste")
+    
+})
